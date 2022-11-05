201e717ceab1f37621b7cb8938c32fdfb2047556
Daisy Marie <miss.daisy@outlook.com.br>
2022-11-05 03:07:17 -0300

    commit 0007

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,52 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
+import locale
 
 # Create your views here.
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    grouped_posts = {}
+    for post in posts:
+        pub = timezone.template_localtime(post.published_date)
+        locale.setlocale(locale.LC_ALL, 'pt_BR.utf8')
+        pub = pub.strftime("%d de %B de %Y")
+        if pub in grouped_posts.keys():
+            grouped_posts[pub].append(post)
+        else:
+            grouped_posts[pub] = [post]
+    
+    return render(request, 'blog/post_list.html', {'grouped_posts': grouped_posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+        return render(request, 'blog/post_form.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+        return render(request, 'blog/post_form.html', {'form': form})
